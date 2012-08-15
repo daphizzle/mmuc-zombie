1644d02e599918252617201d0454e62669c0f7c8
daniel.puschmann@gmx.de <daniel.puschmann@gmx.de@f7f792ec-e51e-48f8-f984-432e1c7ef49f>
2012-08-15 08:40:36 +0000

    First map Test showing Saarbrücken

---
+++ trunk/mmuc-zombie/mmuc-zombie/mapTest.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Controls.Maps;
+using System.Device.Location;
+
+namespace mmuc_zombie
+{
+    public partial class mapTest : PhoneApplicationPage
+    {
+        GeoCoordinateWatcher watcher;
+        public mapTest()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            // Reinitialize the GeoCoordinateWatcher
+            watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+            watcher.MovementThreshold = 100;//distance in metres
+
+            // Add event handlers for StatusChanged and PositionChanged events
+            watcher.StatusChanged += new EventHandler<GeoPositionStatusChangedEventArgs>(watcher_StatusChanged);
+            watcher.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(watcher_PositionChanged);
+
+            // Start data acquisition
+            watcher.Start();
+
+            //hide button
+            btnStart.Visibility = Visibility.Collapsed;
+            button1.Visibility = Visibility.Visible;
+            button2.Visibility = Visibility.Visible;
+        }
+
+
+
+
+        /// <summary>
+        /// Custom method called from the PositionChanged event handler
+        /// </summary>
+        /// <param name="e"></param>
+        void MyPositionChanged(GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            //set fake position for testing purpose
+            e.Position.Location = new GeoCoordinate(49.233, 7);
+
+
+            // Update the map to show the current location
+            mapMain.SetView(new LocationRect(e.Position.Location, 1.0, 1.0));
+
+            //update pushpin location and show
+            MapLayer.SetPosition(ppLocation, e.Position.Location);
+            ppLocation.Visibility = System.Windows.Visibility.Visible;
+
+
+        }
+
+        /// <summary>
+        /// Custom method called from the StatusChanged event handler
+        /// </summary>
+        /// <param name="e"></param>
+        void MyStatusChanged(GeoPositionStatusChangedEventArgs e)
+        {
+            switch (e.Status)
+            {
+                case GeoPositionStatus.Disabled:
+                    // The location service is disabled or unsupported.
+                    // Alert the user
+                    tbStatus.Text = "sorry we can’t find you on this device";
+                    break;
+                case GeoPositionStatus.Initializing:
+                    // The location service is initializing.
+                    // Disable the Start Location button
+                    tbStatus.Text = "looking for you…";
+                    break;
+                case GeoPositionStatus.NoData:
+                    // The location service is working, but it cannot get location data
+                    // Alert the user and enable the Stop Location button
+                    tbStatus.Text = "can’t find you yet…";
+                    ResetMap();
+
+                    break;
+                case GeoPositionStatus.Ready:
+                    // The location service is working and is receiving location data
+                    // Show the current position and enable the Stop Location button
+                    tbStatus.Text = "found you!";
+                    break;
+
+            }
+        }
+
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            mapMain.ZoomLevel++;
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            mapMain.ZoomLevel--;
+        }
+
+
+        void ResetMap()
+        {
+            mapMain.SetView(new LocationRect(new GeoCoordinate(0,0,0), 10.0, 10.0));
+
+            //update pushpin location and show
+            MapLayer.SetPosition(ppLocation, new GeoCoordinate(0, 0, 0));
+            ppLocation.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Handler for the StatusChanged event. This invokes MyStatusChanged on the UI thread and
+        /// passes the GeoPositionStatusChangedEventArgs
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void watcher_StatusChanged(object sender, GeoPositionStatusChangedEventArgs e)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() => MyStatusChanged(e));
+
+        }
+
+        /// <summary>
+        /// Handler for the PositionChanged event. This invokes MyStatusChanged on the UI thread and
+        /// passes the GeoPositionStatusChangedEventArgs
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() => MyPositionChanged(e));
+        }
+
+        #endregion
+
+
+
+
+    }
+}
