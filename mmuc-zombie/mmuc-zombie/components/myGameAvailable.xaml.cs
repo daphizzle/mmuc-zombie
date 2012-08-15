0c0fa8563256d5a7c465568fcf71ed076586cd15
leespinn@gmail.com <leespinn@gmail.com@f7f792ec-e51e-48f8-f984-432e1c7ef49f>
2012-08-15 23:50:50 +0000

    - myGames by using an UserControl (items are loaded from .xaml, it should be loaded automatically from .xaml.cs)   just for testing i created a do-nothing class (GameTmp.cs) - Background and logo images - links over each menu option, and navigation through pages.

---
+++ mmuc-zombie/mmuc-zombie/components/myGameAvailable.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace mmuc_zombie.components
+{
+    public partial class myGameAvailable : UserControl
+    {
+        public myGameAvailable()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ mmuc-zombie/mmuc-zombie/myGames.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.UserData;
+using mmuc_zombie.app.model;
+
+
+namespace mmuc_zombie
+{
+    public partial class Page1 : PhoneApplicationPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //SearchContacts();
+        }
+
+        //private void SearchContacts()
+        //{
+        //    List<GameTmp> games = new List<GameTmp>(3);
+        //    games.Add(new GameTmp("Zombie 1", new DateTime(2012, 08, 15), new DateTime(2012, 08, 15)));
+        //    ContactResultsData.DataContext = games;
+        //}
+
+        //private void ContactResultsData_Tap(object sender, GestureEventArgs e)
+        //{
+        //    App.con = ((sender as ListBox).SelectedValue as Contact);
+
+        //    NavigationService.Navigate(new Uri("/ContactDetails.xaml", UriKind.Relative));
+        //}
+    }
+}
