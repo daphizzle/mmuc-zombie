a0eccb4bed103463f17f3eeba7014c29b27c5c87
jo.lahann@googlemail.com <jo.lahann@googlemail.com@f7f792ec-e51e-48f8-f984-432e1c7ef49f>
2012-08-16 09:25:52 +0000

    models

---
+++ trunk/mmuc-zombie/mmuc-zombie/models/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Parse;
+
+public class User : IParseUser, IParseObject
+{
+
+    public string Id { get; set; }
+    public string UserName{get;set;}
+    public string Password{get;set;}
+    
+    public void addFriend (User u){
+        Friend f=new Friend();
+        f.user = Id;
+        f.friend = u.Id;
+        var parse = new Driver();
+        parse.Objects.Save(f);
+       
+    }
+
+    public void create(){
+        var parse = new Driver();
+        parse.Objects.Save(this,r =>
+        {      
+           if (r.Success)
+        {
+           Id = r.Data.Id;
+        }
+        else
+        {
+            //log r.Error.Message;
+        }
+        });
+
+        
+    }
+}
