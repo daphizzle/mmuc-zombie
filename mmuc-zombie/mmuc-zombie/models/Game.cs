6083d5c3de81a73d79854b53fa1f8e67ca994d31
jo.lahann@googlemail.com <jo.lahann@googlemail.com@f7f792ec-e51e-48f8-f984-432e1c7ef49f>
2012-08-16 09:25:52 +0000

    models

---
+++ mmuc-zombie/mmuc-zombie/models/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Parse;
+
+
+public class Game : ParseObject
+{
+    public Boolean private_mode{get;set;}
+    public int min_participants{get;set;}
+    public int max_participants{ get; set; }
+    public int radius { get; set; }
+    public int zombie_quota { get; set; }
+    public DateTime start { get; set; }
+    public DateTime end { get; set; }
+    
+
+    
+
+
+
+
+}
